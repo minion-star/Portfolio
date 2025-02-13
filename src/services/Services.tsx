cef16ca79ff34aaa8544e38c81a868d4179f6397
minion-star <williamalderette799@gmail.com>
2025-02-13 09:58:36 -0600

    bug fix

--- src/services/Services.tsx
+++ src/services/Services.tsx
@@ -5,9 +5,10 @@ const Services = () =>{
         <div id="services" className="w-full bg-[#31065A]">
             <div className="w-full text-center mb-8 md:mb-12 bg-[#170550]">
                 <h2 className="text-white text-3xl font-semibold border-b-2 border-white inline-block pb-2">
-                My <span className="text-orange-500">Skills</span>
+                My <span className="text-orange-500">Services</span>
                 </h2>
             </div>
+
         </div>
     );
 }
