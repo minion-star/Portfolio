2df4401ba5b32e7c92d9c4d7198bbcabcd1b7e68
minion-star <williamalderette799@gmail.com>
2025-02-13 02:36:19 -0600

    about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from './header/Header'
+import AboutMe from './aboutme/aboutme'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header/>
+      <AboutMe/>
     </>
   )
 }
---
+++ src/aboutme/aboutme.tsx
@@ -0,0 +1,13 @@
+
+
+const AboutMe = () => {
+    return (
+        <div id="about" className="bg-[#31065A] flex top-0 left-0 w-full">
+            <div className="flex items-center">
+                <img src="banner.png" alt="Banner" />
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
