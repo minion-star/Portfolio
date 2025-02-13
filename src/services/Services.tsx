0d946b5be6e56e19b6b9456129685e70b65afdf2
minion-star <williamalderette799@gmail.com>
2025-02-13 11:59:41 -0600

    update

--- src/services/Services.tsx
+++ src/services/Services.tsx
@@ -18,7 +18,7 @@ const Services = () =>{
                 >
                     {/* Service */}
                     <div className="w-full md:w-1/3 text-center md:text-left px-6 mt-8 md:mt-0 bg-white mr-3 flex flex-col items-center rounded-2xl h-120">
-                        <img src={computer} className="h-16 w-16 m-4" />
+                        <img src={computer} className="h-16 w-16 m-16" />
                         <h3 className="text-[#31065A] font-bold m-2">Web Development</h3>
                         <p className="text-[#31065A]">I provide custom web development services, building scalable, secure, and 
                             high-performance websites using React, TypeScript, Node.js, and MySQL. From e-commerce platforms to enterprise solutions, 
@@ -27,13 +27,13 @@ const Services = () =>{
                         </p>
                     </div>
                     <div className="w-full md:w-1/3 text-center md:text-left px-6 mt-8 md:mt-0 bg-white mr-3 flex flex-col items-center rounded-2xl h-120">
-                        <img src={mobile} className="h-16 w-16 m-4"/>
+                        <img src={mobile} className="h-16 w-16 m-16"/>
                         <h4 className="text-[#31065A] font-bold m-2">Apps Development</h4>
                         <p className="text-[#31065A]">I create high-performance, user-friendly mobile applications tailored to your business needs. I specialize in iOS and Android development, ensuring seamless functionality, intuitive design, and cutting-edge technology to enhance user experience.
                         </p>
                     </div>
                     <div className="w-full md:w-1/3 text-center md:text-left px-6 mt-8 md:mt-0 bg-white mr-3 flex flex-col items-center rounded-2xl h-120">
-                        <img src={digitalmarketing} className="h-16 w-16 m-4" />
+                        <img src={digitalmarketing} className="h-16 w-16 m-16" />
                         <h4 className="text-[#31065A] font-bold m-2" >Digital Marketing</h4>
                         <p className="text-[#31065A]">
                         I help businesses grow online with targeted SEO, social media marketing, PPC campaigns, and content marketing. My data-driven strategies enhance brand visibility, drive traffic, and boost conversions, ensuring maximum ROI.
