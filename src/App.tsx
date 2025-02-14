f3a9f2646cad2a1fbd105c6395b7396911157b11
minion-star <williamalderette799@gmail.com>
2025-02-13 18:03:48 -0600

    complete footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Services from './services/Services'
 import Skills from './skills/Skills'
 import Portfolio from './portfolio/Portfolio'
 import Contact from './contact/Contact'
+import Footer from './footer/Footer'
 import './App.css'
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Skills/>
       <Portfolio/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
---
+++ src/footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="w-full bg-[#31065A] flex items-center flex-col align-middle p-8">
+            <p className="text-white text-center">Copyright &copy;  2025. All rights reserved.</p>
+        </div>
+    );
+}
+
+
+export default Footer;
